b83e9e8f0c5b44088e64666f67d8100a15c8f382
williandersonsanchez <56261980+williandersonsanchez@users.noreply.github.com>
2019-10-07 15:04:47 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+    port: process.env.PORT || 8080,
+    // db: process.env.MONGODB || 'mongodb://localhost:27017/node',
+    // SECRET_TOKEN: 'miclavedetokens'
+};
